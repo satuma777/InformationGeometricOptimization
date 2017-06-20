2635f55c652b654656205431019267a24a57052b
S.Shota <satuma184@gmail.com>
2017-06-20 18:27:48 +0900

    implementation of natural gradient updater for gaussian distribution

--- model.py
+++ model.py
@@ -19,52 +19,63 @@ class ProbabilityDistribution(object):
 
 
 class MultiVariableGaussian(ProbabilityDistribution):
-    def __init__(self, dim, mean=None, covariance_matrix=None, stepsize=None, xp=np):
+    def __init__(self, dim, mean=None, var=None, stepsize=None, xp=np):
         self.dim = dim
         self.mean = mean
-        self.covar_mat = covariance_matrix
+        self.var = var
         self.stepsize = stepsize
         self.xp = xp
         self.model_class = 'Gaussian'
         
         if self.mean is None:
             self.mean = xp.zeros(dim)
-        if self.covar_mat is None:
-            self.covar_mat = xp.identity(dim)
+        if self.var is None:
+            self.var = xp.identity(dim)
         if self.stepsize is None:
             self.stepsize = 1.
         
-        assert self.mean.size == dim and self.covar_mat.size == dim * dim, \
+        assert self.mean.size == dim and self.var.size == dim * dim, \
             "Invalid value that dimensions DON'T match."
     
     def sampling(self, pop_size):
-        sqrt_comat = self.xp.linalg.cholesky(self.covar_mat)
+        sqrt_comat = self.xp.linalg.cholesky(self.var)
         sample = self.mean + np.dot(self.xp.random.normal(0., self.stepsize, (pop_size, self.dim)), sqrt_comat)
         return sample
     
     def get_param(self):
-        return self.mean, self.covar_mat, self.stepsize
+        return self.mean, self.var, self.stepsize
     
-    def set_param(self, mean, covariance_matrix, stepsize):
-        assert mean.size == dim and covariance_matrix.size == dim * dim, \
+    def set_param(self, mean=None, var=None, stepsize=None):
+        assert mean.size == dim and var.size == dim * dim, \
             "Invalid value that dimensions DON'T match."
         
-        self.mean = mean
-        self.covar_mat = covariance_matrix
-        self.stepsize = stepsize
+        if mean is None:
+            self.mean = xp.zeros(dim)
+        else:
+            self.mean = mean
+            
+        if var is None:
+            self.var = xp.identity(dim)
+        else:
+            self.var = var
+            
+        if stepsize is None:
+            self.stepsize = 1.
+        else:
+            self.stepsize = stepsize
     
     def log_likelihood(self, sample):
         xp = self.xp
         pop_size = sample.shape[0]
         deviation = sample - self.mean
-        comats = xp.array([self.stepsize * self.stepsize * self.covar_mat for _ in xrange(pop_size)])
+        comats = xp.array([self.stepsize * self.stepsize * self.var for _ in xrange(pop_size)])
         
         try:
             Cinv_der = xp.linalg.solve(comats, deviation)
-            log_Cdet = log(xp.linalg.det(self.covar_mat))
+            log_Cdet = log(xp.linalg.det(self.var))
         except AttributeError:
             Cinv_der = xp.array(np.linalg.solve(comats, deviation))
-            log_Cdet = log(np.linalg.det(self.covar_mat))
+            log_Cdet = log(np.linalg.det(self.var))
         
         loglikelihood = -0.5 * (self.dim * log(2 * pi) + log_Cdet + xp.diag(xp.dot(Cinv_der, deviation.T)))
         
--- updater.py
+++ updater.py
@@ -5,43 +5,27 @@ from math import log, pi
 
 class Updater(object):
     """Base class of all updater."""
-    def setup(self, distribution):
+    def setup(self, distribution, weight_function, lr):
         self.target = distribution
         self.t = 0
-        self._hooks = collections.OrderedDict()
+        self.w_func = weight_function
+        self.lr = lr
+        
+        if not callable(self.w_func):
+            raise TypeError('weight function is NOT callable.')
 
     def update(self):
         raise NotImplementedError()
 
-    def add_hook(self, hook, name=None):
-        if not callable(hook):
-            raise TypeError('hook function is not callable')
-        if not hasattr(self, '_hooks'):
-            raise RuntimeError('call `setup` method before `add_hook` method')
-
-        if name is None:
-            name = hook.name
-        if name in self._hooks:
-            raise KeyError('hook %s already exists' % name)
-        
-        self._hooks[name] = hook
-
-    def call_hooks(self):
-        """Invokes hook functions in registration order."""
-        for hook in self._hooks.itervalues():
-            self._call_hook(hook)
-
-    def _call_hook(self, hook):
-        hook(self, self.target.get_param())
-  
 
 class NaturalGradientUpdater(Updater):
     def __init__(self):
         super(Updater, self).__init__()
     
-    def update(self, weight, sample):
-        self.call_hooks()
+    def update(self, fitness, sample):
         self.t += 1
+        
+        weight = self.w_func(fitness)
         
         if self.target.model_class in 'Gaussian':
             self.gaussian_param_update(weight, sample)
@@ -49,26 +33,32 @@ class NaturalGradientUpdater(Updater):
             self.bernouil_param_update(weight, sample)
     
     def gaussian_param_update(self, weight, sample):
-        grad_m, grad_C = self._compute_natural_grad_gaussian(weight, sample)
+        #TODO:Implementaion of Comulative Step-size Update
+        #TODO:Implementaion of rank-one Update
         
-        pass
+        mean, var = self.target.get_param()
+        grad_m, grad_var = self._compute_natural_grad_gaussian(weight, sample, mean, var)
+       
+        new_mean = mean + self.lr['mean'] * grad_m
+        new_var = var + self.lr['var'] * grad_var
+        
+        self.target.set_param(new_mean, new_var)
     
     def bernouil_param_update(self, weight, sample):
+        # TODO:Implementaion of Population-based increment learning
         pass
 
-    def _compute_natural_grad_gaussian(self, weight, sample):
-        mean = self.target.mean
-        covar_mat = self.target.covar_mat
-        
+    def _compute_natural_grad_gaussian(self, weight, sample, mean, var):
         derivation = sample - mean
         w_der = weight * derivation.T
         grad_m = w_der.sum(axis=1)
 
         if self.target.model_class in 'Isotropic':
-            grad_C =
+            norm_w_der = xp.diag(xp.dot(w_der, w_der.T))
+            grad_var = (xp.sum(weight * norm_w_der) / self.target.dim) - (xp.sum(weight) * var)
         elif self.target.model_class in 'Separable':
-            grad_C =
+            grad_var = (w_der * derivation.T).sum(axis=1) - (weights.sum() * var)
         else:
-            grad_C = np.dot(w_der, derivation) - weight.sum() * covar_mat
+            grad_var = xp.dot(w_der, derivation) - (weight.sum() * var)
         
-        return grad_m, grad_C
+        return grad_m, grad_var
