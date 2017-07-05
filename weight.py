3276aa672cc99a7aec2d16f0f8835014dbcb428d
S.Shota <satuma184@gmail.com>
2017-07-05 20:04:17 +0900

    implementation of quantile-based weight
    - Reference : S. Shirakawa, Y. Akimoto, K. Ouchi, and K. Ohara, “Sample
    Reuse in the Covariance Matrix Adaptation Evolution Strategy Based on
    Importance Sampling,” in Proceedings of the 2015 on Genetic and
    Evolutionary Computation Conference - GECCO ’15, 2015, pp. 305–312.

---
+++ weight.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+
+def cma_like_weight(q_plus, q_minus, xp):
+    weight_plus = -2. * xp.log(2. * qplus)
+    weight_plus[xp.where(q_plus < 0.5)] = 0
+    
+    if q_minus is not None:
+        weight_minus = -2. * xp.log(2. * q_minus)
+        weight_minus[xp.where(q_minus < 0.5)] = 0
+        
+        q_plus_truncated = xp.where(q_plus < 0.5, q_plus, 0.5)
+        q_minus_truncated = xp.where(q_minus < 0.5, q_minus, 0.5)
+
+        diff_idx = xp.where(q_plus != q_minus)
+        
+        weight_plus[diff_idx] = (q_plus_truncated[diff_idx] * weight_plus[diff_idx]
+                                 - q_minus_truncated[diff_idx] * weight_minus[diff_idx]
+                                 + 2. * (q_plus_truncated[diff_idx] + q_minus_truncated[diff_idx]))
+        
+        weight_plus[diff_idx] /= q_plus[diff_idx] - q_minus[diff_idx]
+        
+    return weight_plus
+
+
+class QuantileBasedWeight(object):
+    def __init__(self, minimization=True, tie_case=False, non_increasing_function=cma_like_weight, xp=np):
+        self.min = minimization
+        self.tie_case = tie_case
+        self.non_inc_func = non_increasing_function
+        self.xp = xp
+    
+    def __call__(self, evaluation):
+        pop_size = evaluation.size()
+        q_plus = None
+        q_minus = None
+        
+        if self.min:
+            q_plus = xp.array([evaluation[xp.where(evaluation <= eval)].size() for eval in evaluation])
+            q_plus /= pop_size
+            if self.tie_case:
+                q_minus = xp.array([evaluation[xp.where(evaluation < eval)].size() for eval in evaluation])
+                q_minus /= pop_size
+        
+        else:
+            q_plus = xp.array([evaluation[xp.where(evaluation >= eval)].size() for eval in evaluation])
+            q_plus /= pop_size
+            if self.tie_case:
+                q_minus = xp.array([evaluation[xp.where(evaluation > eval)].size() for eval in evaluation])
+                q_minus /= pop_size
+        
+        return self.non_inc_func(q_plus, q_minus, self.xp)
+
+#TODO: LebesgueMeasureBasedWeight [Akimoto2012(GECCO2012)]
